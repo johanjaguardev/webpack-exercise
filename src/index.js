298b3789a79f95a07dad791ff8aa34bdd9aa9150
Johan Vargas <johanvargasdev@gmail.com>
2022-02-09 11:47:05 -0500

    remove console log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Template from '@templates/Template.js'
 import '@styles/main.css'
 import '@styles/variables.scss'
-console.Console.log('hola')
 
 (async function App() {
   const main = null || document.getElementById('main')
